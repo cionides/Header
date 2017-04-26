e2f3716eef88d66bf6539f3b5f708314d3eac425
Christina <christina@Christinas-MacBook-Pro.local>
2017-04-26 11:07:39 -0500

    Adding styles to weather component

--- src/app/weather.component.ts
+++ src/app/weather.component.ts
@@ -5,13 +5,15 @@ import { Weather } from './weather.interface';
 @Component({
   selector: 'weather-component',
   template: `
-  <div *ngIf="weather">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <div class="navbar-header" *ngIf="weather">
     {{ weather.location.name}},
+    {{ weather.location.region}}
     {{ weather.current.temp_f}} F
     {{ weather.current.condition.text}}
     <img src="{{weather.current.condition.icon}}">
   </div>
-  `
+  `,
 })
 
 export class WeatherComponent implements OnInit {
