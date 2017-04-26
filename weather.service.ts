35de9d1213d5acc782ee091e10a5252107083af4
Christina <christina@Christinas-MacBook-Pro.local>
2017-04-26 09:42:04 -0500

    exporting classes from new file

---
+++ weather.service.ts
@@ -0,0 +1,2 @@
+export * from './src/app/weather.service';
+export * from './src/app/weather.interface';
--- weather.ts
+++ weather.ts
@@ -1,3 +1,1 @@
 export * from './src/app/weather.component';
-export * from './src/app/weather.service';
-export * from './src/app/weather.interface';
