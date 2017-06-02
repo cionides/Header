d89a436f2ffca8fc86d64b01de8f7fbf1befa63d
Will <animnook@yahoo.com>
2017-06-02 16:35:06 -0500

    header makeover

--- src/app/weather.component.ts
+++ src/app/weather.component.ts
@@ -5,34 +5,7 @@ import { Forecast } from './forecast.interface';
 
 @Component({
   selector: 'weather-component',
-  template: `<div id="parent" style="display:flex; float:right; padding-top:10px;">
-    <div id="wide" style="width:200;">
-        <div style="align:right; display: table;" *ngIf="forecast">
-            <div style="display: table-row; float: left;">
-                <div style="float: left; display: table-cell;">
-                    <img src="{{forecast.current.condition.icon}}">
-                </div>
-                <div style="float: left; display: table-cell;">
-                    <div id="weather">
-                        {{ forecast.location.name }} {{ forecast.current.temp_f}} °F
-                        <br> {{ forecast.current.condition.text}}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div id="narrow" style="width:200px;">
-        <div style="float: right; width: 100%; display: table;" *ngIf="forecast">
-            <div style="display: table-row; float: right;">
-                <button style="width:150px;" type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Forecast</button>
-                <div style="color: white;" id="demo" class="collapse">
-                    Tomorrow {{ forecast.forecast.forecastday[0].day.avgtemp_f}} °F
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-`,
+  templateUrl: 'weather.component.html',
   styleUrls: ['./header.component.css']
 })
 
